a92b1d2b60ae8197cb82f6629c6125d3e32986eb
Soel Labat <soel.labat@outlook.com>
2020-11-11 23:37:45 +0100

    Almost done, need to fix a few things

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Restaurant.destroy_all
+
+epicure = { name: "Epicure", address: "Paris 75013", category: "french" }
+mamamia = { name: "Mamamia", address: "Paris 75004", category: "italian" }
+whocares = { name: "Whocares", address: "Paris 75015", category: "belgian" }
+asian_touch = { name: "Asian Touch", address: "Paris 75013", category: "chinese" }
+mimo = { name: "Mimo", address: "75018", category: "japanese" }
+
+[ epicure, mamamia, whocares, asian_touch, mimo ].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+end
